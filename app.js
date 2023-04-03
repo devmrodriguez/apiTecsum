fb7e20cae1ec8fa01271342f345d59ac79d0b87c
devmrodriguez <marioguzman_95@hotmail.com>
2023-04-03 13:51:16 -0600

    Agregando comando start

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 
 /* Declaraciones iniciales */
 const app = express();
+/* Asignando el puerto */
+const port = process.env.PORT || 3000;
 
 /* Configuraciones de mi servidor express */
 app.use(express.json());
@@ -14,16 +16,8 @@ app.use(express.static(__dirname + '/public'));
 app.use(cors());
 
 /* Enrutamiento */
-app.use("/", require("./routes"));
+app.use("/", require("./src/routes"));
 
-
-/* Puerto de heroku */
-let port = process.env.PORT;
-/* si no se ha configurado el puerto o el puerto es una cadena vacía */
-if (port == null || port == "") {
-  // entonces usaremos el puerto 3000
-  port = 3000;
-}
 app.listen(port, function() {
     console.log(`Server started on port ${port}`);
 });
---
+++ src/routes.js
@@ -0,0 +1,40 @@
+require('dotenv').config(); 
+const router = require('express').Router();
+const correoNotificacion = require('../funciones/notificacionCorreo');
+
+/* Funciones */
+
+/* Ruta raíz */
+router.route('/').get(function (req, resp) {
+    resp.send('API Tecnología y Suministros S.A. de C.V.')
+});
+
+router.route('/api').get(function (req, resp){
+    resp.send('API Tecnología y Suministros S.A. de C.V.')
+});
+
+/* Ruta para el envío de correos */
+router.route("/contacto")
+    .post(function (req, res) {
+
+        try{
+            let nombre = req.body.nombre;
+            let email = req.body.email;
+            let telefono = req.body.telefono;
+            let empresa = req.body.empresa;
+            let mensaje = req.body.mensaje;
+        
+            var correoAdmin = process.env.EMAIL_USER;
+            /* Enviando correo a administrador */
+            correoNotificacion(0, nombre, correoAdmin, mensaje, telefono, empresa); 
+            /* Enviando correo a cliente */
+            correoNotificacion(1, nombre, email, mensaje, telefono, empresa); 
+            res.status(200).send("Correo enviado correctamente");
+        }catch(error){
+            console.log(error);
+            res.status(500).send("Error al enviar el correo");
+        }
+    });
+
+
+module.exports = router;
